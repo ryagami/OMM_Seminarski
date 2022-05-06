82b7dd7982afe6a407b894afce3ca78c8617ed1a
Stefan Stankovic <ryagami@users.noreply.github.com>
2022-05-06 21:28:30 +0200

    Added support for basic animation

---
+++ GUI.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+window = tk.Tk()
+
+#canvas1 = tk.Canvas(window, width=500, height=500)
+greeting = tk.Label(text="Hello, Tkinter")
+#canvas1.pack()
+greeting.pack()
+window.mainloop()
+
--- animation.py
+++ animation.py
@@ -1,20 +1,21 @@
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import matplotlib.animation as manimation
 
 
-def animate(data):
+def animate(data, step):
     FFMpegWriter = manimation.writers['ffmpeg']
     metadata = dict(title='opruga', artist='Matplotlib', comment='-')
-    writer = FFMpegWriter(fps=15, metadata=metadata)
+    writer = FFMpegWriter(fps=120, metadata=metadata)
 
     fig = plt.figure()
 
+    plt.plot(data["range"], data["position"])
     opruga, = plt.plot([], [], 'ro', markersize=10)
 
-    with writer.saving(fig, "spring_animation.mp4"):
-        for i in data["range"]:
-            x0 = data["range"][i]
+    with writer.saving(fig, "spring_animation.mp4", 100):
+        for i in range(0, len(data["range"]), step):
+            x0 = 0.5  # data["range"][i]
             y0 = data["position"][i]
             opruga.set_data(x0, y0)
             writer.grab_frame()
