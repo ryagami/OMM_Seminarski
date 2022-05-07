81dbc362ea0dc6dcec50480cee0be8d4d7601db1
Stefan Stankovic <ryagami@users.noreply.github.com>
2022-05-07 05:27:53 +0200

    Added GUI.

--- GUI.py
+++ GUI.py
@@ -1,10 +1,129 @@
-import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+from tkinter.messagebox import askyesno
+import matplotlib as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+from matplotlib.figure import Figure
 
-window = tk.Tk()
+import opruga
 
-#canvas1 = tk.Canvas(window, width=500, height=500)
-greeting = tk.Label(text="Hello, Tkinter")
-#canvas1.pack()
-greeting.pack()
-window.mainloop()
+plt.use("TkAgg")
 
+if __name__ == '__main__':
+    root = Tk()
+    root.title("Strong spring oscillation graphs")
+    root.columnconfigure((1, 3, 5), weight=1)
+    root.rowconfigure(4, weight=1)
+
+
+    def onCLick_plot():
+        c = entry_c.get()
+        k = entry_k.get()
+        j = entry_j.get()
+        f = entry_f.get()
+        alpha = entry_alpha.get()
+        beta = entry_beta.get()
+        c = float(c) if c != "" else 0.0
+        k = float(k) if k != "" else 0.3
+        j = float(j) if j != "" else 0.04
+        f = f if f != "" else "0"
+        f = "print(" + f + ")"
+        alpha = float(alpha) if alpha != "" else 0.1
+        beta = float(beta) if beta != "" else 0.0
+
+        o = opruga.opruga_rk(c, k, j, f, alpha, beta, 100, 0.001, 4)
+
+        tickEraseValue = askyesno("Erase canvas", "Do you want to clear canvases?")
+
+        if tickEraseValue:
+            fig1.clear()
+            global plot1
+            plot1 = fig1.add_subplot(111)
+            fig2.clear()
+            global plot2
+            plot2 = fig2.add_subplot(111)
+            fig3.clear()
+            global plot3
+            plot3 = fig3.add_subplot(111)
+
+        plot1.plot(o["range"], o["position"])
+        canvas1.draw()
+
+        plot2.plot(o["range"], o["speed"])
+        canvas2.draw()
+
+        plot3.plot(o["position"], o["speed"])
+        canvas3.draw()
+
+        # print(c, k, j, f, alpha, beta)
+
+
+    label_c = ttk.Label(root, text="c")
+    label_c.grid(row=0, column=0)
+    entry_c = ttk.Entry(root)
+    entry_c.grid(row=0, column=1, padx=5, pady=5, sticky=W + E)
+
+    label_k = ttk.Label(root, text="k")
+    label_k.grid(row=0, column=2)
+    entry_k = ttk.Entry(root)
+    entry_k.grid(row=0, column=3, padx=5, pady=5, sticky=W + E)
+
+    label_j = ttk.Label(root, text="j")
+    label_j.grid(row=0, column=4)
+    entry_j = ttk.Entry(root)
+    entry_j.grid(row=0, column=5, padx=5, pady=5, sticky=W + E)
+
+    label_f = ttk.Label(root, text="f(t)")
+    label_f.grid(row=1, column=0)
+    entry_f = ttk.Entry(root)
+    entry_f.grid(row=1, column=1, padx=5, pady=5, sticky=W + E)
+
+    label_alpha = ttk.Label(root, text="alpha")
+    label_alpha.grid(row=1, column=2)
+    entry_alpha = ttk.Entry(root)
+    entry_alpha.grid(row=1, column=3, padx=5, pady=5, sticky=W + E)
+
+    label_beta = ttk.Label(root, text="beta")
+    label_beta.grid(row=1, column=4)
+    entry_beta = ttk.Entry(root)
+    entry_beta.grid(row=1, column=5, padx=5, pady=5, sticky=W + E)
+
+    button_plot = ttk.Button(root, text="Plot", command=onCLick_plot)
+    button_plot.grid(row=3, column=2, padx=5, pady=5, columnspan=2)
+
+    plot_tabs = ttk.Notebook(root)
+
+    plot_tab1 = ttk.Frame(plot_tabs)
+    plot_tabs.add(plot_tab1, text="Grafik položaja", sticky="news")
+    plot_tab1.columnconfigure(0, weight=1)
+    fig1 = Figure(figsize=(5, 5), dpi=100)
+    canvas1 = FigureCanvasTkAgg(fig1, plot_tab1)
+    plot1 = fig1.add_subplot(111)
+    canvas1.get_tk_widget().pack(side=TOP, fill=BOTH, expand=True)# grid(row=0, column=0, sticky="news")
+    canvas1.draw()
+
+    plot_tab2 = ttk.Frame(plot_tabs)
+    plot_tabs.add(plot_tab2, text="Grafik brzine", sticky="news")
+    plot_tab2.columnconfigure(0, weight=1)
+    fig2 = Figure(figsize=(5, 5), dpi=100)
+    canvas2 = FigureCanvasTkAgg(fig2, plot_tab2)
+    plot2 = fig2.add_subplot(111)
+    canvas2.draw()
+    canvas2.get_tk_widget().pack(side=TOP, fill=BOTH, expand=True)# grid(row=0, column=0, sticky="news")
+
+    plot_tab3 = ttk.Frame(plot_tabs)
+    plot_tabs.add(plot_tab3, text="Fazni grafik",sticky="news")
+    plot_tab3.columnconfigure(0, weight=1)
+    fig3 = Figure(figsize=(5, 5), dpi=100)
+    canvas3 = FigureCanvasTkAgg(fig3, plot_tab3)
+    plot3 = fig3.add_subplot(111)
+    canvas3.draw()
+    canvas3.get_tk_widget().pack(side=TOP, fill=BOTH, expand=True)# grid(row=0, column=0, sticky=W+E)
+
+    plot_tabs.grid(row=4, column=0, columnspan=6, sticky="news")
+
+    # tickEraseValue = IntVar()
+    # tickEraseCanvas = ttk.Checkbutton(root, text="Erase canvases before drawing?", variable=tickEraseValue)
+    # tickEraseCanvas.grid(row=5, column=0, columnspan=6, sticky="w")
+
+    root.mainloop()
--- opruga.py
+++ opruga.py
@@ -1,9 +1,12 @@
-import numpy as np
+import sys
+from numpy import *
 import matplotlib.pyplot as plt
-# import time
+from io import StringIO
+from contextlib import redirect_stdout
 
 import animation as anim
-import GUI
+
+set_printoptions(threshold=sys.maxsize)
 
 
 def opruga_rk(c, k, l, f, a, b, t_range, step, m):  # tol, ind):
@@ -13,21 +16,27 @@ def opruga_rk(c, k, l, f, a, b, t_range, step, m):  # tol, ind):
     # m je red metode, tol je preciznost
     # ind je indikator da li zelimo modifikovanu RK metodu
 
-    def fu(t1, u1, v1):
-        return v1
+    def fu(t, u, v):
+        return v
 
-    def fv(t1, u1, v1):
-        return f(t1) - c * v1 - k * u1 - l * pow(u1, 3)
+    def fv(t, u, v):
+        out = StringIO()
+        with redirect_stdout(out):
+            exec(f)
+            # ft = out.getvalue().strip("][\\\n").split(" ")
+            # ft = [float(x) for x in ft] if len(ft) > 1 else float(ft[0])
+            # return ft - c * v - k * u - l * pow(u, 3)
+            return float(out.getvalue()) - c * v - k * u - l * pow(u, 3)
 
-    t = np.arange(0, t_range + step, step)
+    t = arange(0, t_range + step, step)
     n = len(t)
 
-    u = np.zeros(n)
+    u = zeros(n)
     u[0] = a
-    v = np.zeros(n)
+    v = zeros(n)
     v[0] = b
 
-    beta = np.zeros((int(m), int(m)))
+    beta = zeros((int(m), int(m)))
 
     # izbor reda Runge-Kutta metode
     match m:
@@ -36,35 +45,35 @@ def opruga_rk(c, k, l, f, a, b, t_range, step, m):  # tol, ind):
             beta = 1
             p = 1
         case 2:
-            alpha = np.array([0, 1])
+            alpha = array([0, 1])
             beta[0, 0] = 1
             beta[1, 0] = 1
-            p = np.array([1 / 2, 1 / 2])
+            p = array([1 / 2, 1 / 2])
         case 2.1:
-            alpha = np.array([0, 1 / 2])
+            alpha = array([0, 1 / 2])
             beta[0, 0] = 1
             beta[1, 0] = 1 / 2
-            p = np.array([0, 1])
+            p = array([0, 1])
         case 3:
-            alpha = np.array([0, 1 / 2, 1])
+            alpha = array([0, 1 / 2, 1])
             beta[0, 0] = 1
             beta[1, 0] = 1 / 2
             beta[2, 0] = -1
             beta[2, 1] = 2
-            p = np.array([1 / 6, 2 / 3, 1 / 6])
+            p = array([1 / 6, 2 / 3, 1 / 6])
         case _:
-            alpha = np.array([0, 1 / 2, 1 / 2, 1])
+            alpha = array([0, 1 / 2, 1 / 2, 1])
             beta[0, 0] = 1
             beta[1, 0] = 1 / 2
             beta[2, 1] = 1 / 2
             beta[3, 2] = 1
-            p = np.array([1 / 6, 1 / 3, 1 / 3, 1 / 6])
+            p = array([1 / 6, 1 / 3, 1 / 3, 1 / 6])
 
     # print(beta)
 
     for i in range(n - 1):
-        ku = np.zeros(int(m))
-        kv = np.zeros(int(m))
+        ku = zeros(int(m))
+        kv = zeros(int(m))
 
         for j in range(int(m)):
             ku[j] = step * fu(t[i] + alpha[j] * step, u[i] + beta[j, :] @ ku, v[i] + beta[j, :] @ ku.transpose())
@@ -73,28 +82,28 @@ def opruga_rk(c, k, l, f, a, b, t_range, step, m):  # tol, ind):
         u[i + 1] = u[i] + p @ ku
         v[i + 1] = v[i] + p @ kv
 
-    fig, (ax1, ax2, ax3, ax4) = plt.subplots(1, 4)
-    fig.tight_layout()
-
-    ax1.plot(t, u)
-    ax1.set_title("Grafik funkcije položaja")
-    ax1.set_xlabel("t")
-    ax1.set_ylabel("x")
-
-    ax2.plot(t, v)
-    ax2.set_title("Grafik funkcije brzine")
-    ax2.set_xlabel("t")
-    ax2.set_ylabel("x'")
-
-    ax3.plot(u, v)
-    ax3.set_title("Fazni grafik")
-    ax3.set_xlabel("x")
-    ax3.set_ylabel("x'")
-
-    ax4.plot(t, fv(t, u, v))
-    ax4.set_title("Grafik ubrzanja")
-    ax4.set_xlabel("t")
-    ax4.set_ylabel("x''")
+    # fig, (ax1, ax2, ax3, ax4) = plt.subplots(1, 4)
+    # fig.tight_layout()
+    #
+    # ax1.plot(t, u)
+    # ax1.set_title("Grafik funkcije položaja")
+    # ax1.set_xlabel("t")
+    # ax1.set_ylabel("x")
+    #
+    # ax2.plot(t, v)
+    # ax2.set_title("Grafik funkcije brzine")
+    # ax2.set_xlabel("t")
+    # ax2.set_ylabel("x'")
+    #
+    # ax3.plot(u, v)
+    # ax3.set_title("Fazni grafik")
+    # ax3.set_xlabel("x")
+    # ax3.set_ylabel("x'")
+    #
+    # ax4.plot(t, fv(t, u, v))
+    # ax4.set_title("Grafik ubrzanja")
+    # ax4.set_xlabel("t")
+    # ax4.set_ylabel("x''")
 
     # plt.show()
 
@@ -102,16 +111,7 @@ def opruga_rk(c, k, l, f, a, b, t_range, step, m):  # tol, ind):
         "range": t,
         "position": u,
         "speed": v,
-        "acceleration": fv(t, u, v)
+        "acceleration": list(map(fv, t, u, v))
     }
 
     return o
-
-
-if __name__ == '__main__':
-    def f(t):
-        return -9.81
-
-
-    o = opruga_rk(0.0, 0.3, 0.04, f, 1.0, 0.0, 40, 0.001, 4)
-    anim.animate(o, 30)
